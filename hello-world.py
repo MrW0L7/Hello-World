d9187caf65e5dc980406660fcc631382bdb63694
MrW0L7 <92720176+MrW0L7@users.noreply.github.com>
2024-10-17 13:32:47 +0200

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,29 @@
+print("Hello Python World! ")
+
+message = 'Hello Python World'
+print(message)
+
+message = 'I love Python'
+print(message)
+
+message = ' I am 30 year old'
+print(message)
+
+# Underscores works for variable but spaces does not
+greeting_message = 'Hello'
+print(greeting_message)
+
+# Python does not do spell checking thus it still prints variable 
+mesage = 'Hello Python Crash Course reader!'
+print(mesage)
+
+teacher = "Larry Page"
+print(teacher)
+
+str = 'String'
+print(str)
+
+# Using quotes
+mess = 'I love python "insert quotes!" '
+print(mess)
+
